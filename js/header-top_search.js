d75fb37ed5713e5cdbb831f3f21060e379fd44cb
Griffith88 <dobrvn@gmail.com>
2021-08-25 08:47:14 +0300

    Diploma: Header fully adapted

--- js/header-top_search.js
+++ js/header-top_search.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', function () {
     var burger = document.querySelector('.header-top__burger')
     var search_form = document.querySelector('.header-top__search')
     var search_cancel = document.querySelector('.header-top__search-cancel')
+    var header_top = document.querySelector('.header-top')
     console.log(search)
     if (btn.contains(event.target) || search.contains(event.target))  {
       search.classList.add('search-input-active')
@@ -13,6 +14,7 @@ window.addEventListener('DOMContentLoaded', function () {
       burger.classList.add('header-top__burger-hidden')
       search_form.classList.add('header-top__search-full')
       search_cancel.classList.add('header-top__search-canceled')
+      header_top.classList.add('header-top__searching')
     }
     else if (search_cancel.contains(event.target)) {
       search.value = ""
@@ -23,6 +25,7 @@ window.addEventListener('DOMContentLoaded', function () {
       burger.classList.remove('header-top__burger-hidden')
       search_form.classList.remove('header-top__search-full')
       search_cancel.classList.remove('header-top__search-canceled')
+      header_top.classList.remove('header-top__searching')
     }
   });
 })
