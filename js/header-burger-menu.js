4421b10d52814297d33c722531828afa43d6892f
Vladimir Dobrokhotov <dobrvn@gmail.com>
2021-08-25 21:16:46 +0300

    started burger menu

---
+++ js/header-burger-menu.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('click', function(event) {
+    var burger=document.querySelector('.header-top__burger')
+    var menu = document.querySelector('.header-top__menu')
+    if (burger.contains(event.target)) {
+      menu.classList.add('header-top__menu-active')
+    }
+    else {
+      menu.classList.remove('header-top__menu-active')
+    }
+  });
+})
