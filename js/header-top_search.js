53eca0202e190927c3b10c9bb0e2d6348ddda24b
Griffith88 <dobrvn@gmail.com>
2021-09-01 15:58:03 +0300

    ready to show teacher

--- js/header-top_search.js
+++ js/header-top_search.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('click', function(event) {
-        btn = document.querySelector('.header-top__search-form')
+        btn_form = document.querySelector('.header-top__search-form')
         btn_submit = document.querySelector('.header-top__search-submit')
         icon = document.querySelector('.header-top__search-btn')
         submit_block = document.querySelector('.submit-block')
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', function() {
         burger = document.querySelector('.header-top__burger')
         if (search_cancel.contains(event.target)) {
             search.value = ""
-        } else if (btn.contains(event.target) || search.contains(event.target)) {
+        } else if (icon.contains(event.target) || search.contains(event.target)) {
             icon.classList.add('is-hidden')
             search.classList.add('is-visible')
             btn_submit.classList.add('is-visible')
@@ -18,8 +18,9 @@ window.addEventListener('DOMContentLoaded', function() {
             search_cancel.classList.add('is-visible__mobile')
             logo.classList.add('is-hidden__mobile')
             burger.classList.add('is-hidden__mobile')
-            btn.classList.add('is-active__mobile-vertical')
-            btn.classList.add('is-active__mobile')
+            btn_form.classList.add('is-active__mobile-vertical')
+            btn_form.classList.add('is-active__mobile')
+            btn_form.classList.add('is-visilbe-flex')
         } else {
             icon.classList.remove('is-hidden')
             search.classList.remove('is-visible')
@@ -27,8 +28,9 @@ window.addEventListener('DOMContentLoaded', function() {
             search_cancel.classList.remove('is-visible__mobile')
             logo.classList.remove('is-hidden__mobile')
             burger.classList.remove('is-hidden__mobile')
-            btn.classList.remove('is-active__mobile-vertical')
-            btn.classList.remove('is-active__mobile')
+            btn_form.classList.remove('is-active__mobile-vertical')
+            btn_form.classList.remove('is-active__mobile')
+            btn_form.classList.remove('is-visilbe-flex')
             submit_block.classList.remove('.is-visible-flex')
         }
     });
