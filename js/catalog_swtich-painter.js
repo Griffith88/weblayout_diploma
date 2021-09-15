a87d6c12bed619bc1717e7b5fbb30b432b3c9ccb
Griffith88 <dobrvn@gmail.com>
2021-09-15 11:29:08 +0300

    catalog done with corrections

--- js/catalog_swtich-painter.js
+++ js/catalog_swtich-painter.js
@@ -1,11 +1,20 @@
 window.addEventListener('DOMContentLoaded', function () {
   const btnList = document.querySelectorAll('.accordion-bottom-btn')
-  const cardList = document.querySelectorAll('.catolog__left-content')
   const dominicoContent = document.querySelector('.dominico-content')
   const emptyContent = document.querySelector('.empty')
   btnList.forEach(function(btn) {
     btn.addEventListener('click', function(event) {
       if (btn.contains(event.target)) {
+        for (alterBtn of btnList) {
+          if (alterBtn == btn) {
+            console.log('добавляю фокус')
+            alterBtn.dataset.focused = 'true'
+          }
+          else {
+            console.log('убираю фокус')
+            alterBtn.dataset.focused = 'false'
+          }
+        }
         if (btn.classList.contains('dominico-btn')) {
           dominicoContent.style.display = 'flex'
           emptyContent.style.display = 'none'
